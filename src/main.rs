8d61e94df27680ebfce185c3542c2d41982aa5f1
donbasta <faddilafarras@gmail.com>
2024-04-07 02:25:23 +0700

    feat: mvp ls

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use::std::io;
+use std::io;
 // use::std::fs::File;
-use::std::fs;
-use::std::env;
+use std::path::Path;
+use std::fs;
+use std::env;
 
 const MANUAL: &str = "echo: repeats input
 cat: concatenate files
@@ -11,12 +12,13 @@ grep: matches text in files
 ";
 
 fn main() {
-    println!("Hello, world!");
     let current_dir = env::current_dir().expect("Failed to get current directory");
+    let current_dir_path = Path::new(&current_dir);
+
 
     loop {
         let mut command = String::new();
-        
+
         io::stdin().read_line(&mut command).expect("Failed to read line");
         command = command.trim_start_matches(|c| c == ' ').to_string();
 
@@ -25,7 +27,7 @@ fn main() {
 
         if tokens.len() == 0 {
             continue;
-        }   
+        }
 
         print!(">> ");
         match tokens[0] {
@@ -38,8 +40,42 @@ fn main() {
                     println!("{}", &command[itr..command.len()]);
                 }
             },
+            "cd" => {
+                // if tokens.len() > 2 {
+                //     println!("Too many arguments");
+                // } else if tokens.len() == 2 {
+                //     let dest = tokens[1];
+                //     if dest == ".." {
+                //         //go back to parent
+                //         current_dir_path = Path::new(current_dir_path).parent().unwrap();
+                //     } else if tokens[1].starts_with('/') {
+                //         // case 1. dest startswith / (absolute path)
+                //         match fs::symlink_metadata(tokens[1]) {
+                //             Ok(metadata) => {
+                //                 if metadata.is_file() {
+                //                     println!("File exists, but not a path. Can't change directory");
+                //                 } else if metadata.is_dir() {
+                //                     println!("Directory exists");
+                //                     current_dir_path = Path::new(tokens[1]);
+                //                 } else {
+                //                     println!("Not a file nor a directory");
+                //                 }
+                //             },
+                //             Err(err) => eprintln!("Error: {}", err)
+                //         }
+                //     } else if tokens[1].starts_with("./") {
+                //         // case 2. dest startswith . (relative path)
+                //         match fs::canonicalize(tokens[1]) {
+                //             Ok(absolute_path) => {
+                //                 println!("Absolute path: {:?}", absolute_path);
+                //             }
+                //             Err(err) => eprintln!("Error: {}", err)
+                //         }
+                //     }
+                // }
+            }
             "ls" => {
-                if let Ok(entries) = fs::read_dir(&current_dir) {
+                if let Ok(entries) = fs::read_dir(&current_dir_path) {
                     println!("Files in current directory:");
                     for entry in entries {
                         if let Ok(entry) = entry {
@@ -50,7 +86,6 @@ fn main() {
                 } else {
                     println!("Failed to read directory contents");
                 }
-                println!("to do doing ls");
             },
             "cat" => {
                 // let mut file = File::open()
