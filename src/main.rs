3686f3b1b0b4ef21d24dbebf3635e442befabed5
donbasta <faddilafarras@gmail.com>
2024-04-07 02:50:08 +0700

    mvp cd

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,8 @@ grep: matches text in files
 
 fn main() {
     let current_dir = env::current_dir().expect("Failed to get current directory");
-    let current_dir_path = Path::new(&current_dir);
-
+    let mut current_dir_path = Path::new(&current_dir);
+    // let mut command: String;
 
     loop {
         let mut command = String::new();
@@ -29,7 +29,6 @@ fn main() {
             continue;
         }
 
-        print!(">> ");
         match tokens[0] {
             "echo" => {
                 if tokens.len() >= 2 {
@@ -40,40 +39,53 @@ fn main() {
                     println!("{}", &command[itr..command.len()]);
                 }
             },
+            "pwd" => {
+                println!("{}", current_dir_path.display());
+            },
             "cd" => {
-                // if tokens.len() > 2 {
-                //     println!("Too many arguments");
-                // } else if tokens.len() == 2 {
-                //     let dest = tokens[1];
-                //     if dest == ".." {
-                //         //go back to parent
-                //         current_dir_path = Path::new(current_dir_path).parent().unwrap();
-                //     } else if tokens[1].starts_with('/') {
-                //         // case 1. dest startswith / (absolute path)
-                //         match fs::symlink_metadata(tokens[1]) {
-                //             Ok(metadata) => {
-                //                 if metadata.is_file() {
-                //                     println!("File exists, but not a path. Can't change directory");
-                //                 } else if metadata.is_dir() {
-                //                     println!("Directory exists");
-                //                     current_dir_path = Path::new(tokens[1]);
-                //                 } else {
-                //                     println!("Not a file nor a directory");
-                //                 }
-                //             },
-                //             Err(err) => eprintln!("Error: {}", err)
-                //         }
-                //     } else if tokens[1].starts_with("./") {
-                //         // case 2. dest startswith . (relative path)
-                //         match fs::canonicalize(tokens[1]) {
-                //             Ok(absolute_path) => {
-                //                 println!("Absolute path: {:?}", absolute_path);
-                //             }
-                //             Err(err) => eprintln!("Error: {}", err)
-                //         }
-                //     }
-                // }
-            }
+                if tokens.len() > 2 {
+                    println!("Too many arguments");
+                } else if tokens.len() == 2 {
+                    let dest = tokens[1];
+                    if dest == ".." {
+                        //go back to parent
+                        current_dir_path = Path::new(current_dir_path).parent().unwrap();
+                    } else if dest.starts_with('/') { // case 1. dest startswith / (absolute path)
+                        match fs::symlink_metadata(dest) {
+                            Ok(metadata) => {
+                                if metadata.is_file() {
+                                    println!("File exists, but not a path. Can't change directory");
+                                } else if metadata.is_dir() {
+                                    println!("Directory exists");
+                                    current_dir_path = Path::new(dest.to_string().leak());
+                                } else {
+                                    println!("Not a file nor a directory");
+                                }
+                            },
+                            Err(err) => eprintln!("Error: {}", err)
+                        }
+                    } else if dest.starts_with("./") {
+                        // case 2. dest startswith . (relative path)
+                        let absolute_path = current_dir_path.join(Path::new(dest.to_string().leak()));
+                        println!("{}", absolute_path.display());
+                        match fs::symlink_metadata(absolute_path.clone()) {
+                            Ok(metadata) => {
+                                if metadata.is_file() {
+                                    println!("File exists, but not a path. Can't change directory");
+                                } else if metadata.is_dir() {
+                                    println!("Directory exists");
+                                    current_dir_path = Path::new(absolute_path.to_string_lossy().into_owned().leak());
+                                } else {
+                                    println!("Not a file nor a directory");
+                                }
+                            },
+                            Err(err) => eprintln!("Error: {}", err)
+                        }
+                    }
+                } else {
+                    println!("No such file or directory");
+                }
+            },
             "ls" => {
                 if let Ok(entries) = fs::read_dir(&current_dir_path) {
                     println!("Files in current directory:");
