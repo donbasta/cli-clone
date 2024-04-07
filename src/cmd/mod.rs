033a78bc0afb622a20fc213d317825fc01724a8b
donbasta <faddilafarras@gmail.com>
2024-04-08 06:08:30 +0700

    refactor: handling if path in run_cd has . or ..

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -208,6 +208,19 @@ impl CMDVariables {
                                 abs_path.push(&dest.to_string().leak()[2..]);
                                 abs_path
                             }
+                            dest if dest.starts_with("../") => {
+                                let mut abs_path = self.current_dir_path.clone();
+                                match abs_path.parent() {
+                                    Some(c) => {
+                                        abs_path = c.to_path_buf();
+                                    }
+                                    None => {
+                                        return Err("Can't move to parent directory".to_string());
+                                    }
+                                }
+                                abs_path.push(&dest.to_string().leak()[3..]);
+                                abs_path
+                            }
                             &_ => {
                                 let mut abs_path = self.current_dir_path.clone();
                                 abs_path.push(dest.to_string());
@@ -217,7 +230,9 @@ impl CMDVariables {
                         match fs::symlink_metadata(absolute_path.clone()) {
                             Ok(metadata) => {
                                 if metadata.is_file() {
-                                    println!("File exists, but not a path. Can't change directory");
+                                    println!(
+                                        "File exists, but not a directory. Can't change directory"
+                                    );
                                 } else if metadata.is_dir() {
                                     self.current_dir_path = absolute_path;
                                 } else {
